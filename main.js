af59eca86598ae442fdae131f125de2b34d2a350
Pikrpan <54728861+Pikrpan@users.noreply.github.com>
2024-04-05 11:10:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//Vytvoř funkci s názvem vytvorElementy
+
+
+//Tělo funkce
+
+
+  //Vytvoř proměnnou, do které zapíšeš náhodnou hodnotu od jedné do pěti
+  
+  //Vytvoř cyklus for, který se má provést tolikrát, kolik je hodnota proměnné s náhodnou hodnotou
+
+
+    //Tělo cyklu
+
+
+      //Vytvoř HTML element h2
+      
+      //Vytvořenému h2 elementu přiřaď hodnotu proměnné s náhodnou hodnotou
+      
+      //Vlož h2 element do div elementu, který má třídu "grid-container"
+
+    //Konec těla cyklu
+               
+
+//Konec těla funkce
+
